0704117c6fa80c015abedcfef3728849b83b0e6c
Diana Le <le.diana.d@gmail.com>
2020-09-13 19:49:29 -0500

    Add dynamic totals
    Updated color palette. Added instructions and dynamic vote totals for each candidate when each state is updated.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # usa-electoral-map-2020
  Interactive map of USA states with the ability to click on each state to change the electoral vote totals for each candidate
+
+# Credits
+Electoral votes from: https://www.archives.gov/electoral-college/2020
+SVG map from: https://simplemaps.com/resources/customize-us-svg
--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const states = document.querySelectorAll('.state');
+const current = document.querySelector('.current-state');
+const trump = document.querySelector('.trump .votes');
+const biden = document.querySelector('.biden .votes');
 let trumpVotes = [];
 let bidenVotes = [];
 
@@ -17,7 +20,7 @@ function claimState(e) {
             votes: Number.parseInt(this.dataset.electorate)
          };
          trumpVotes.push(trumpArray);
-         // console.log(trumpVotes);
+         //console.log(trumpVotes);
          break;
       case 'trump' :
          this.dataset.candidate = 'biden';
@@ -39,6 +42,7 @@ function claimState(e) {
          this.dataset.candidate = '';
          break;
    }
+   updateVoteTotals();
 
 }
 
@@ -46,18 +50,46 @@ function claimState(e) {
 function removeVotes(state, arrayToChange) {
    // first find the index of the item in the array
    const stateIndex = arrayToChange.findIndex(usa => usa.state === state);
-   console.log(stateIndex);
-   // make a new array without that item
+   //console.log(stateIndex);
+   // Modify the original array
    arrayToChange.splice(stateIndex, 1);
-   // return new array
 }
 
 /* Update Candidate Votes */
 function updateVoteTotals() {
+   let trumpTotal = 0;
+   for(let t = 0; t < trumpVotes.length; t++) {
+      trumpTotal += trumpVotes[t].votes;
+   }
+   //console.log('Trump total: ' + trumpTotal);
+   trump.textContent = `${trumpTotal} Votes`;
 
+   let bidenTotal = 0;
+   for(let b = 0; b < bidenVotes.length; b++) {
+      bidenTotal += bidenVotes[b].votes;
+   }
+   //console.log('Biden total: ' + bidenTotal);
+   biden.textContent = `${bidenTotal} Votes`;
+}
+
+/* check electoral vote total */
+function checkElectoral() {
+   let total = 0;
+   for(let i = 0; i < states.length; i++) {
+      total += Number.parseInt(states[i].dataset.electorate);
+   }
+   console.log(total);
+}
+
+function handleStateInfo(e) {
+   e.preventDefault();
+   const stateName = this.dataset.state;
+   const stateElectorates = this.dataset.electorate;
+   current.textContent = `${stateName}: ${stateElectorates} votes`;
 }
 
 // Listen for state clicks to change colors and votes
 for (let i = 0; i < states.length; i++) {
-   states[i].addEventListener('click', claimState);
+   states[i].addEventListener('dblclick', claimState);
+   states[i].addEventListener('click', handleStateInfo);
 }
