b2ec1c477b09ef79ec98e73089be0bf85b3d69e3
Diana Le <le.diana.d@gmail.com>
2020-09-26 19:16:08 -0500

    Added Maine/Nebraska logic
    Added logic for users to manually update the votes for each candidate for Maine and Nebraska. Refactored the sorting functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 const states = document.querySelectorAll('.state');
+const maine = document.querySelector('state[data-state="maine"]');
+const nebraska = document.querySelector('state[data-state="nebraska"]');
+const maineTotal = 4;
+const nebraskaTotal = 5;
+const maineError = document.querySelector('.maine-error');
+const nebraskaError = document.querySelector('.nebraska-error');
+const updateMaine = document.querySelector('#update-maine');
+const updateNebraska = document.querySelector('#update-nebraska');
+const exceptionInputs = document.querySelectorAll('input[type="number"]');
 const current = document.querySelector('.current-state');
 const trump = document.querySelector('.trump .votes');
 const biden = document.querySelector('.biden .votes');
@@ -19,10 +28,10 @@ Change votes to Trump or to Biden.
 First double-click goes to Trump, second changes to Biden, third goes back to neutral.
 Add votes to their respective arrays.
 Remove votes from their respective arrays.
+Sort arrays.
 Update vote totals at the end.
 */
 function claimState(e) {
-   // console.log(this.dataset.electorate);
    e.preventDefault();
    switch (e.currentTarget.dataset.candidate) {
       case '' :
@@ -50,37 +59,29 @@ function claimState(e) {
          // remove from Biden
          removeVotes(e.currentTarget.dataset.state, bidenVotes);
          break;
+      case 'exception' :
+         break;
       default:
          e.currentTarget.dataset.candidate = '';
          break;
    }
-   // Sort vote objects
-   trumpVotes.sort(function(a, b) {
-      let stateA = a.state.toLowerCase();
-      let stateB = b.state.toLowerCase();
-      if (stateA < stateB) {
-         return -1;
-      }
-      if (stateA > stateB) {
-         return 1;
-      }
-      return 0;
-   });
-
-   bidenVotes.sort(function(a, b) {
-      let stateA = a.state.toLowerCase();
-      let stateB = b.state.toLowerCase();
-      if (stateA < stateB) {
-         return -1;
-      }
-      if (stateA > stateB) {
-         return 1;
-      }
-      return 0;
-   });
-
    updateVoteTotals();
 
+}
+
+// sort arrays for each candidate alphabetically by state
+function sortCandidateArray(candidateArray) {
+   candidateArray.sort(function(a, b) {
+      let stateA = a.state.toLowerCase();
+      let stateB = b.state.toLowerCase();
+      if (stateA < stateB) {
+         return -1;
+      }
+      if (stateA > stateB) {
+         return 1;
+      }
+      return 0;
+   });
 }
 
 /* Remove votes from candidate when the state color changes */
@@ -88,29 +89,36 @@ function removeVotes(state, arrayToChange) {
    // first find the index of the item in the array
    const stateIndex = arrayToChange.findIndex(usa => usa.state === state);
    // Modify the original array
-   arrayToChange.splice(stateIndex, 1);
+   //console.log(stateIndex);
+   if (stateIndex !== -1) {
+      arrayToChange.splice(stateIndex, 1);
+   }
 }
 
 /* Update Candidate Votes upon each state double-click */
 function updateVoteTotals() {
    let trumpTotal = 0;
    let trumpAllStates = '';
+   sortCandidateArray(trumpVotes);
    for(let t = 0; t < trumpVotes.length; t++) {
       trumpTotal += trumpVotes[t].votes;
       trumpAllStates += `<div class="table-row"><div>${trumpVotes[t].state}</div><div>${trumpVotes[t].votes}</div></div>`;
    }
 
+   // Update the HTML with the states and percentage to 270
    trump.textContent = `${trumpTotal} Votes`;
    trumpPercentage.style.width = `${trumpTotal / 270 * 100}%`;
    trumpTable.innerHTML = trumpAllStates;
  
    let bidenTotal = 0;
    let bidenAllStates = '';
+   sortCandidateArray(bidenVotes);
    for(let b = 0; b < bidenVotes.length; b++) {
       bidenTotal += bidenVotes[b].votes;
       bidenAllStates += `<div class="table-row"><div>${bidenVotes[b].state}</div><div>${bidenVotes[b].votes}</div></div>`;
    }
 
+   // Update the HTML with the states and percentage to 270
    biden.textContent = `${bidenTotal} Votes`;
    bidenPercentage.style.width = `${bidenTotal / 270 * 100}%`;
    bidenTable.innerHTML = bidenAllStates;
@@ -123,6 +131,96 @@ function checkElectoral() {
       total += Number.parseInt(states[i].dataset.electorate);
    }
    console.log(total);
+}
+
+// Restrict inputs on the exception states
+exceptionInputs.forEach(exception => {
+   exception.addEventListener('keypress', handleExceptionInputs);
+});
+
+// Only allow integers in the number input fields
+function handleExceptionInputs(e) {
+   //console.log(e.key);
+   if (!parseInt(e.key) && e.key !== '0') {
+      e.preventDefault();
+   }
+}
+
+// Check the totals for Maine and Nebraska
+function handleMaineTotals() {
+   let maineTrump = parseInt(document.querySelector('#maine-trump').value);
+   let maineBiden = parseInt(document.querySelector('#maine-biden').value);
+   let maineTrumpArray = {};
+   let maineBidenArray = {};
+   maineError.textContent = ``;
+
+   // If either box is empty, set value to 0
+   if (!maineTrump) {
+      maineTrump = 0;
+   }
+   if (!maineBiden) {
+      maineBiden = 0;
+   }
+
+   let maineInputsTotal = maineTrump + maineBiden;
+
+   if (maineInputsTotal > maineTotal) {
+      maineError.textContent = `The total of ${maineInputsTotal} is higher than the max of ${maineTotal}`;
+   } else {
+      removeVotes('Maine', trumpVotes);
+      maineTrumpArray = {
+         state: 'Maine',
+         votes: maineTrump
+      };
+      trumpVotes.push(maineTrumpArray);
+
+      removeVotes('Maine', bidenVotes);
+      maineBidenArray = {
+         state: 'Maine',
+         votes: maineBiden
+      };
+      bidenVotes.push(maineBidenArray);
+
+      updateVoteTotals();
+   }
+}
+
+function handleNebraskaTotals() {
+   let nebraskaTrump = parseInt(document.querySelector('#nebraska-trump').value);
+   let nebraskaBiden = parseInt(document.querySelector('#nebraska-biden').value);
+   let nebraskaTrumpArray = {};
+   let nebraskaBidenArray = {};
+   nebraskaError.textContent = ``;
+
+   // If either box is empty, set value to 0
+   if (!nebraskaTrump) {
+      nebraskaTrump = 0;
+   }
+   if (!nebraskaBiden) {
+      nebraskaBiden = 0;
+   }
+
+   let nebraskaInputsTotal = nebraskaTrump + nebraskaBiden;
+
+   if (nebraskaInputsTotal > nebraskaTotal) {
+      nebraskaError.textContent = `The total of ${nebraskaInputsTotal} is higher than the max of ${nebraskaTotal}`;
+   } else {
+      removeVotes('Nebraska', trumpVotes);
+      nebraskaTrumpArray = {
+         state: 'Nebraska',
+         votes: nebraskaTrump
+      };
+      trumpVotes.push(nebraskaTrumpArray);
+
+      removeVotes('Nebraska', bidenVotes);
+      nebraskaBidenArray = {
+         state: 'Nebraska',
+         votes: nebraskaBiden
+      };
+      bidenVotes.push(nebraskaBidenArray);
+
+      updateVoteTotals();
+   }
 }
 
 /* Show the current state and electoral count on single click */
@@ -159,6 +257,10 @@ function handleBidenTable(e) {
       e.currentTarget.setAttribute('aria-expanded', 'true');
    }
 }
+
+// Listen for updates to Maine / Nebraska states
+updateMaine.addEventListener('click', handleMaineTotals);
+updateNebraska.addEventListener('click', handleNebraskaTotals);
 
 // Listen for button clicks to collapse / expand states table
 trumpTableButton.addEventListener('click', handleTrumpTable);
